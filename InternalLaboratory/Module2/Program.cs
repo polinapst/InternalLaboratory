6d52d4b197a9410e02b7e3336e8b6c66151d27bc
polinapst <36256340+polinapst@users.noreply.github.com>
2022-07-09 21:21:37 +0600

    Added IL solution, Module 2, finished M2 task2

---
+++ InternalLaboratory/Module2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Module2.Search.MaxIntElement(new[] { 1, int.MaxValue }));
---
+++ InternalLaboratory/Module2/Search.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+    static class Search
+    {
+        /// <summary>
+        /// Задание 2: Реализовать рекурсивный алгоритм поиска максимального элемента в неотсортированном целочисленом массиве.
+        /// </summary>
+        public static int MaxIntElement(int[] a)
+        {
+            return MaxIntElement(a, 0, a.Length, int.MinValue);
+        }
+
+        public static int MaxIntElement(int[] a, int i, int size, int currentMax)
+        {
+            if (i < size)
+            {
+                if (a[i] > currentMax)
+                {
+                    currentMax = a[i];
+                }
+
+                return MaxIntElement(a, i + 1, a.Length, currentMax);
+            }
+            
+            return currentMax;
+        }
+    }
+}
