749f2cbf238c7cf51821e22288a4e345d8ff6fc5
polinatuzankina <polinatuzankina@gmail.com>
2022-07-09 23:25:38 +0600

    Added method FindNextBiggerNumber from M2 Task5

--- InternalLaboratory/Module2/Methods.cs
+++ InternalLaboratory/Module2/Methods.cs
@@ -16,6 +16,9 @@ namespace Module2
             return FindMaxIntElement(a, 0, a.Length, int.MinValue);
         }
 
+        /// <summary>
+        /// Задание 2: Реализовать рекурсивный алгоритм поиска максимального элемента в неотсортированном целочисленом массиве.
+        /// </summary>
         public static int FindMaxIntElement(int[] a, int i, int size, int currentMax)
         {
             if (i < size)
@@ -83,6 +86,43 @@ namespace Module2
             }
 
             return sb.ToString();
+        }
+
+        /// <summary>
+        /// Реализовать метод FindNextBiggerNumber, который принимает положительное целое число и возвращает ближайшее наибольшее целое, состоящее из цифр исходного числа, и null (или -1), если такого числа не существует.
+        /// </summary>
+        public static int FindNextBiggerNumber(int n)
+        {
+            char[] arrayOfDigitChars = n.ToString().ToCharArray();
+
+            if (arrayOfDigitChars.Length == 1)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < arrayOfDigitChars.Length; i++)
+            {
+                for (int j = i + 1; j < arrayOfDigitChars.Length; j++)
+                {
+                    if (arrayOfDigitChars[i].CompareTo(arrayOfDigitChars[j]) < 0)
+                    {
+                        char temp = arrayOfDigitChars[i];
+                        arrayOfDigitChars[i] = arrayOfDigitChars[j];
+                        arrayOfDigitChars[j] = temp;
+                    }
+                }
+            }
+
+            int result = -1;
+            string s = new string(arrayOfDigitChars);
+            int.TryParse(s, out result);
+            
+            if (result == n)
+            {
+                return -1;
+            }
+
+            return result;
         }
     }
 }
--- InternalLaboratory/Module2/Program.cs
+++ InternalLaboratory/Module2/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine(Module2.Methods.FindMaxIntElement(new[] { 1, int.MaxValue }));
 Console.WriteLine(Module2.Methods.FindSymmetryCenter(new[] { 1.0, 2.0, 1.0, 3.1 }));
 
 Console.WriteLine(Module2.Methods.ConcatNonrepeating("sssssssssssss", "ssssssssssssssssssssssd"));
+
+Console.WriteLine(Module2.Methods.FindNextBiggerNumber(456));
