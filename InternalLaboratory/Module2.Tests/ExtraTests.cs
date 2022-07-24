3c3f369254269f5577a9cecbe293da7919577655
polinatuzankina <polinatuzankina@gmail.com>
2022-07-24 23:06:20 +0600

    Added moq test in Extra/ExtraTests classes

---
+++ InternalLaboratory/Module2.Tests/ExtraTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Module2.Tests
+{
+    internal class ExtraTests
+    {
+        [Test]
+        public void ShouldReturnAListofBooks()
+        {
+            //Arrange
+            var mock = new Mock<IBookCatalogue>();
+            mock.Setup(_bookCatalogue => _bookCatalogue.GetAllBooks()).Returns(GetTestBooks());
+            var bookShop = new Bookshop(mock.Object);
+
+            //Act
+            IEnumerable<Book> result = bookShop.GetBookCatalogue();
+
+            //Assert
+            Assert.AreEqual(result.Count(), GetTestBooks().Count);
+        }
+
+        private List<Book> GetTestBooks()
+        {
+            var books = new List<Book>
+            {
+                new Book { Author = "JRR Tolkien", Title = "Lord od the Rings"},
+                new Book { Author = "Joseph Albahari", Title = "C# 6.0 in a Nutshell"}
+            };
+
+            return books;
+        }
+    }
+}
---
+++ InternalLaboratory/Module2/Extra.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+    internal class Extra
+    {
+
+    }
+
+    public class Bookshop
+    {
+        IBookCatalogue _bookCatalogue;
+
+        public Bookshop(IBookCatalogue bookCatalogue)
+        {
+            _bookCatalogue = bookCatalogue;
+        }
+        public string Name { get; set; }
+
+        public IEnumerable<Book> GetBookCatalogue()
+        {
+            return _bookCatalogue.GetAllBooks();
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    public interface IBookCatalogue
+    {
+        IEnumerable<Book> GetAllBooks();
+    }
+}
