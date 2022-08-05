a6bcad8cca1a8477296b210eff4c7d1c8ce74f98
polinatuzankina <polinatuzankina@gmail.com>
2022-08-05 19:39:38 +0600

    m3 current version

---
+++ InternalLaboratory/Module3.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Module3.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public double ShouldFindNthRoot(double number, int degree, double precison) => NewtonsMethod.FindNthRoot(number, degree, precison);
+    }
+}
---
+++ InternalLaboratory/Module3/NewtonsMethod.cs
@@ -0,0 +1,24 @@
+﻿namespace Module3
+{
+    //public interface IFindNthRoot
+    //{
+     //   public double FindNthRoot(double number, int degree, double precision);
+    //}
+
+    public static class NewtonsMethod
+    {
+        public static double FindNthRoot(double number, int degree, double precision)
+        {
+            double x = number / degree;
+            double x1 = ((degree - 1) * x + (number / Math.Pow(x, degree - 1))) / degree;
+
+            while (Math.Abs(x - x1) > precision)
+            {
+                x = x1;
+                x1 = ((degree - 1) * x + (number / Math.Pow(x, degree - 1))) / degree;
+            }
+
+            return x1;
+        }
+    }
+}
