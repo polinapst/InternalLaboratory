328dd17f18eab3c533ac5cacbc2c5b327368fc33
polinatuzankina <polinatuzankina@gmail.com>
2022-08-21 19:14:25 +0600

    Added Module 4 Euclidean algorithm

---
+++ InternalLaboratory/Module4.Tests/EuclideanAlgorithmTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Module4.Tests
+{
+    [TestFixture]
+    public class EuclideanAlgorithmTests
+    {
+        [Test]
+        public void ShouldThrowArgumentNullException_CalculateGCD() =>
+            Assert.Throws<ArgumentNullException>(() => EuclideanAlgorithm.CalculateGCD(input: Array.Empty<int>()));
+
+        [TestCase(true, 5, 15, 25, ExpectedResult = 5)]
+        [TestCase(true, 0, ExpectedResult = 0)]
+        [TestCase(false, 0, 0, 0, 0, 0, ExpectedResult = 0)]
+        [TestCase(true, 1, 99, 16, ExpectedResult = 1)]
+        [TestCase(false, 100, 40, ExpectedResult = 20)]
+        [TestCase(false, 555, 555, ExpectedResult = 555)]
+        [TestCase(true, -5, 10, ExpectedResult = 5)]
+        [TestCase(false, -100, -40, ExpectedResult = 20)]
+        [TestCase(false, -6, ExpectedResult = 6)]
+        public int ShouldCalculateGCDForSeveralNumbers(bool isEuclidean, params int[] inputNumbers) =>
+            EuclideanAlgorithm.CalculateGCD(isEuclidean, input: inputNumbers);
+
+        [Test]
+        public void ShouldReturnExecitionTime_CalculateGCD()
+        {
+            //Arrange
+            string timeElapsed;
+            int[] inputNumbers = { 5, 6, 7};
+
+            //Act
+            EuclideanAlgorithm.CalculateGCD(out timeElapsed, true, inputNumbers);
+
+            //Assert
+            Assert.IsNotEmpty(timeElapsed);
+        }
+    }
+}
---
+++ InternalLaboratory/Module4/EuclideanAlgorithm.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics;
+
+namespace Module4
+{
+    public static class EuclideanAlgorithm
+    {
+        private static int CalculateGCDEuclidean(int numberA, int numberB)
+        {
+            if (numberB == 0)
+            {
+                return Math.Abs(numberA);
+            }
+
+            return CalculateGCDEuclidean(numberB, numberA % numberB);
+        }
+
+        private static int CalculateGCDBinary(int numberA, int numberB)
+        {
+            numberA = Math.Abs(numberA);
+            numberB = Math.Abs(numberB);
+
+            if (numberA == numberB)
+            {
+                return numberA;
+            }
+                
+            if (numberA == 0)
+            {
+                return numberB;
+            }
+                
+            if (numberB == 0)
+            {
+                return numberA;
+            }
+                
+            if ((~numberA & 1) != 0)
+            {
+                if ((numberB & 1) != 0)
+                {
+                    return CalculateGCDBinary(numberA >> 1, numberB);
+                }
+                    
+                else
+                {
+                    return CalculateGCDBinary(numberA >> 1, numberB >> 1) << 1;
+                }
+                    
+            }
+
+            if ((~numberB & 1) != 0)
+            {
+                return CalculateGCDBinary(numberA, numberB >> 1);
+            }
+                
+            if (numberA > numberB)
+            {
+                return CalculateGCDBinary((numberA - numberB) >> 1, numberB);
+            }
+                
+            return CalculateGCDBinary((numberB - numberA) >> 1, numberA);
+        }
+
+        private static int CalculateGCDForSeveralNumbers(Func<int, int, int> CalculationMethod, params int[] inputArray)
+        {
+            int GCD = CalculationMethod(inputArray[0], inputArray[1]);
+
+            for (int i = 2 ; i < inputArray.Length; i++)
+            {
+                GCD = CalculationMethod(GCD, inputArray[i]);
+            }
+
+            return GCD;
+        }
+
+        public static int CalculateGCD(out string executionTime, bool IsEuclidean, params int[] input)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int methodResult = CalculateGCD(IsEuclidean, input);
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            executionTime = ts.ToString(@"dd\.hh\:mm\:ss\:fffffff");
+            return methodResult;
+        }
+
+        public static int CalculateGCD(bool IsEuclidean = true, params int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(input), "You cannot pass zero arguments");
+            }
+
+            if (input.Length == 1)
+            {
+                return Math.Abs(input[0]);
+            }
+
+            return CalculateGCDForSeveralNumbers(IsEuclidean ? CalculateGCDEuclidean : CalculateGCDBinary, input);
+        }
+    }
+}
